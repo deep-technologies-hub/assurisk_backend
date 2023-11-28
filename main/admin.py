41345c179bfe0a8561770cc7599c582bb726d8cc
Shun Nikiema <shun.nikiema@deeptechnologies.com>
2023-11-28 13:06:40 +0000

    second commit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Medicament, PatientProfile, PharmacieProfile, PrestataireSoinsProfile, Traitement, ReclamationPatient, Prescription, PoliceAssurance
 
-# Register your models here.
+admin.site.register(Medicament)
+admin.site.register(PatientProfile)
+admin.site.register(PharmacieProfile)
+admin.site.register(PrestataireSoinsProfile)
+admin.site.register(Traitement)
+admin.site.register(ReclamationPatient)
+admin.site.register(Prescription)
+admin.site.register(PoliceAssurance)
