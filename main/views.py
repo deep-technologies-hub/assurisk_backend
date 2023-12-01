c441b3bfe3eb992a3c09c3917bac4a8400830d2c
Shun Nikiema <shun.nikiema@deeptechnologies.com>
2023-12-01 09:46:40 +0000

    09:46 01/12/2023

--- main/views.py
+++ main/views.py
@@ -1,4 +1,10 @@
 from rest_framework import viewsets
+from django.contrib.auth.models import User
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import PatientProfile, PrestataireSoinsProfile, PharmacieProfile
+from .serializers import PatientProfileSerializer, PrestataireSoinsProfileSerializer, PharmacieProfileSerializer
+
 from .models import (PatientProfile, PrestataireSoinsProfile, PharmacieProfile,
                      PoliceAssurance, ReclamationPatient, Traitement, Medicament, Prescription)
 from .serializers import (PatientProfileSerializer, PrestataireSoinsProfileSerializer,
@@ -6,17 +12,57 @@ from .serializers import (PatientProfileSerializer, PrestataireSoinsProfileSeria
                           ReclamationPatientSerializer, TraitementSerializer,
                           MedicamentSerializer, PrescriptionSerializer)
 
+
 class PatientProfileViewSet(viewsets.ModelViewSet):
     queryset = PatientProfile.objects.all()
     serializer_class = PatientProfileSerializer
+
+    def create(self, request, *args, **kwargs):
+        user_data = request.data.pop('user')  # Informations de l'utilisateur
+        user = User.objects.create_user(**user_data)
+        patient_serializer = self.get_serializer(data=request.data)
+
+        if patient_serializer.is_valid():
+            patient_serializer.save(user=user)
+            return Response(patient_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            user.delete()  # Supprime l'utilisateur si la création du patient échoue
+            return Response(patient_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class PrestataireSoinsProfileViewSet(viewsets.ModelViewSet):
     queryset = PrestataireSoinsProfile.objects.all()
     serializer_class = PrestataireSoinsProfileSerializer
 
+    def create(self, request, *args, **kwargs):
+        user_data = request.data.pop('user')  # Informations de l'utilisateur
+        user = User.objects.create_user(**user_data)
+        prestataire_serializer = self.get_serializer(data=request.data)
+
+        if prestataire_serializer.is_valid():
+            prestataire_serializer.save(user=user)
+            return Response(prestataire_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            user.delete()  # Supprime l'utilisateur si la création échoue
+            return Response(prestataire_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class PharmacieProfileViewSet(viewsets.ModelViewSet):
     queryset = PharmacieProfile.objects.all()
     serializer_class = PharmacieProfileSerializer
+
+    def create(self, request, *args, **kwargs):
+        user_data = request.data.pop('user')  # Informations de l'utilisateur
+        user = User.objects.create_user(**user_data)
+        pharmacie_serializer = self.get_serializer(data=request.data)
+
+        if pharmacie_serializer.is_valid():
+            pharmacie_serializer.save(user=user)
+            return Response(pharmacie_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            user.delete()  # Supprime l'utilisateur si la création échoue
+            return Response(pharmacie_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class PoliceAssuranceViewSet(viewsets.ModelViewSet):
     queryset = PoliceAssurance.objects.all()
